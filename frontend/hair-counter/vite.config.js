b6f2922923087c2050fa0767945a897c95f49e60
Evasive-6 <albinshiju285@gmail.com>
2025-02-13 15:48:35 +0530

    Update vite.config.js

--- frontend/hair-counter/vite.config.js
+++ frontend/hair-counter/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Bind to 0.0.0.0
-    port: 3000, // You can specify any port here
-    allowedHosts: ['contact-1-hcf4.onrender.com'], // Add your host here
-  }
+
 });
